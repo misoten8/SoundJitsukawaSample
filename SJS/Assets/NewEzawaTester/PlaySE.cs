a808f02d27b366f196f40d66e15a0fda80beafea
EzawaSyuuto <ezawa.syuuto@gmail.com>
2017-10-19 16:29:15 +0900

    no message

---
+++ SJS/Assets/NewEzawaTester/PlaySE.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySE : MonoBehaviour {
+
+    //音声ファイル格納用変数
+    private AudioSource se;
+
+   // [SerializeField]
+    public AudioClip SE_01;
+    public AudioClip SE_02;
+
+    // Use this for initialization
+    void Start () {
+        se = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+
+        //指定のキーが押されたら音声ファイルの再生をする
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            se.PlayOneShot(SE_01);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            se.PlayOneShot(SE_02);
+        }
+    }
+}
---
+++ SJS/Assets/ShadarAudio/Scripts/WaveformRenderer.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveformRenderer : MonoBehaviour
+{
+    [SerializeField]
+    AudioSource audioSource;
+    public RawImage image;
+    [SerializeField]
+    int imageWidth;
+
+    Texture2D texture;
+    float[] _spectrum = new float[50000];
+    public float[] spectrum
+    {
+        get { return _spectrum; }
+    }
+    void Awake()
+    {
+        texture = new Texture2D(imageWidth, 1);
+        texture.SetPixels(Enumerable.Range(0, imageWidth).Select(_ => Color.clear).ToArray());
+        texture.Apply();
+        image.texture = texture;
+    }
+
+    void Update()
+    {
+        audioSource.clip.GetData(_spectrum, audioSource.timeSamples);
+
+        int textureX = 0;
+        int skipSamples = 200;
+        float maxSample = 0;
+
+        for (int i = 0, l = _spectrum.Length; i < l && textureX < imageWidth; i++)
+        {
+            maxSample = Mathf.Max(maxSample, _spectrum[i]);
+
+            if (i % skipSamples == 0)
+            {
+                texture.SetPixel(textureX, 0, new Color(maxSample, 0, 0));
+                maxSample = 0;
+                textureX++;
+            }
+        }
+
+        texture.Apply();
+        
+    }
+}
